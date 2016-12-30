618809955dbf01cfb7ba0f45fcef36086f9bb9ee
Alfons Lange <alfonslange@Alfonss-MacBook-Pro.local>
2016-12-30 17:02:58 +0100

    printing env variable

--- Models/MainContext.cs
+++ Models/MainContext.cs
@@ -16,6 +16,7 @@ namespace EventAppCore.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            Console.WriteLine("Using Environment variable {0}", Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb"));
             optionsBuilder.UseMySql(Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb"));
             //optionsBuilder.UseMySql("Server=localhost;Database=EventApp;Uid=root");
             //optionsBuilder.UseMySql("Database=localdb;Server=127.0.0.1:55954;Uid=azure;Password=6#vWHD_$");
