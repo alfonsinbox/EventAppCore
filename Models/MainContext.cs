43baddad5a62884a9dff1b05677d194b357dedec
Alfons Lange <alfonslange@Alfonss-MacBook-Pro.local>
2016-12-31 12:17:03 +0100

    New connection string

--- Models/MainContext.cs
+++ Models/MainContext.cs
@@ -20,7 +20,7 @@ namespace EventAppCore.Models
             //optionsBuilder.UseMySql(Environment.GetEnvironmenVariable("MYSQLCONNSTR_localdb"));
             //optionsBuilder.UseMySql("Server=localhost;Database=EventApp;Uid=root");
             //optionsBuilder.UseMySql("Database=localdb;Data Source=127.0.0.1:55954;User Id=azure;Password=6#vWHD_$");
-            optionsBuilder.UseMySql("Database=eventappcoredb;Data Source=eu-cdbr-azure-west-a.cloudapp.net;User Id=b400816cb0b397;Password=80b41c16");
+            optionsBuilder.UseMySql("Database=eventappdb;Data Source=eu-cdbr-azure-west-a.cloudapp.net;User Id=b8eec197bc2043;Password=a9669c43");
             Database.Migrate();
         }
 
